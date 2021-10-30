3dc9c1f0d6857ed5fd2187b51e5b098055531f92
Lulu <redbike2014@gmail.com>
2021-10-30 23:05:30 +0100

    testing things out

--- javascripts/paralaxEffect.js
+++ javascripts/paralaxEffect.js
@@ -6,7 +6,7 @@ function paralax(){
 
     // let scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
     let scrollY = document.documentElement.scrollTop
-    scrollY = scrollY/75
+    scrollY = scrollY/10
     let b = document.body
 
     b.setAttribute("Style", "background-position-y: " + scrollY.toString() + "%");
